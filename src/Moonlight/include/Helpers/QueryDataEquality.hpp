29e9cbe2371f1e49b54d97b1d884fd0a3b70cdc7
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-12-16 15:27:56 +0200

    Reduced operator== boilerplate via macros.

--- src/Moonlight/include/Helpers/QueryDataEquality.hpp
+++ src/Moonlight/include/Helpers/QueryDataEquality.hpp
@@ -1,132 +1,29 @@
 #include "QueryData.hpp"
 
-namespace LunarDB::Moonlight::QueryData {
-
-bool operator==(const Create& lhs, const Create& rhs)
-{
-    const auto reduce = [](const Create& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
+#define PROVIDE_EQUALITY_OPERATOR(Type, ...) \
+namespace LunarDB::Moonlight::QueryData { \
+bool operator==(const Type& lhs, const Type& rhs) \
+{ \
+const auto reduce = [](const Type& obj) { return std::tie(__VA_ARGS__); }; \
+return reduce(lhs) == reduce(rhs); \
+} \
 }
 
-bool operator==(const Drop& lhs, const Drop& rhs)
-{
-    const auto reduce = [](const Drop& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Migrate& lhs, const Migrate& rhs)
-{
-    const auto reduce = [](const Migrate& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Truncate& lhs, const Truncate& rhs)
-{
-    const auto reduce = [](const Truncate& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Rename& lhs, const Rename& rhs)
-{
-    const auto reduce = [](const Rename& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Select& lhs, const Select& rhs)
-{
-    const auto reduce = [](const Select& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Insert& lhs, const Insert& rhs)
-{
-    const auto reduce = [](const Insert& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Update& lhs, const Update& rhs)
-{
-    const auto reduce = [](const Update& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Delete& lhs, const Delete& rhs)
-{
-    const auto reduce = [](const Delete& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Lock& lhs, const Lock& rhs)
-{
-    const auto reduce = [](const Lock& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Grant& lhs, const Grant& rhs)
-{
-    const auto reduce = [](const Grant& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Revoke& lhs, const Revoke& rhs)
-{
-    const auto reduce = [](const Revoke& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Commit& lhs, const Commit& rhs)
-{
-    const auto reduce = [](const Commit& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Rollback& lhs, const Rollback& rhs)
-{
-    const auto reduce = [](const Rollback& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const SavePoint& lhs, const SavePoint& rhs)
-{
-    const auto reduce = [](const SavePoint& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Index& lhs, const Index& rhs)
-{
-    const auto reduce = [](const Index& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const Database& lhs, const Database& rhs)
-{
-    const auto reduce = [](const Database& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-bool operator==(const View& lhs, const View& rhs)
-{
-
-    const auto reduce = [](const View& obj) { return true; };
-
-    return reduce(lhs) == reduce(rhs);
-}
-
-} // namespace LunarDB::Moonlight::QueryData
+PROVIDE_EQUALITY_OPERATOR(Create)
+PROVIDE_EQUALITY_OPERATOR(Drop)
+PROVIDE_EQUALITY_OPERATOR(Migrate)
+PROVIDE_EQUALITY_OPERATOR(Truncate)
+PROVIDE_EQUALITY_OPERATOR(Rename)
+PROVIDE_EQUALITY_OPERATOR(Select)
+PROVIDE_EQUALITY_OPERATOR(Insert)
+PROVIDE_EQUALITY_OPERATOR(Update)
+PROVIDE_EQUALITY_OPERATOR(Delete)
+PROVIDE_EQUALITY_OPERATOR(Lock)
+PROVIDE_EQUALITY_OPERATOR(Grant)
+PROVIDE_EQUALITY_OPERATOR(Revoke)
+PROVIDE_EQUALITY_OPERATOR(Commit)
+PROVIDE_EQUALITY_OPERATOR(Rollback)
+PROVIDE_EQUALITY_OPERATOR(SavePoint)
+PROVIDE_EQUALITY_OPERATOR(Index)
+PROVIDE_EQUALITY_OPERATOR(Database)
+PROVIDE_EQUALITY_OPERATOR(View)
