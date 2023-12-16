8407f9d34f2b9cd89420ab716b4d42b4ea509e3c
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-12-16 15:21:13 +0200

    Added reduce lambdas in operator== of QueryData. Further impl required using std::tie(data fields...)

--- src/Moonlight/include/Helpers/QueryDataEquality.hpp
+++ src/Moonlight/include/Helpers/QueryDataEquality.hpp
@@ -4,93 +4,129 @@ namespace LunarDB::Moonlight::QueryData {
 
 bool operator==(const Create& lhs, const Create& rhs)
 {
-    return true;
+    const auto reduce = [](const Create& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Drop& lhs, const Drop& rhs)
 {
-    return true;
+    const auto reduce = [](const Drop& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Migrate& lhs, const Migrate& rhs)
 {
-    return true;
+    const auto reduce = [](const Migrate& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Truncate& lhs, const Truncate& rhs)
 {
-    return true;
+    const auto reduce = [](const Truncate& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Rename& lhs, const Rename& rhs)
 {
-    return true;
+    const auto reduce = [](const Rename& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Select& lhs, const Select& rhs)
 {
-    return true;
+    const auto reduce = [](const Select& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Insert& lhs, const Insert& rhs)
 {
-    return true;
+    const auto reduce = [](const Insert& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Update& lhs, const Update& rhs)
 {
-    return true;
+    const auto reduce = [](const Update& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Delete& lhs, const Delete& rhs)
 {
-    return true;
+    const auto reduce = [](const Delete& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Lock& lhs, const Lock& rhs)
 {
-    return true;
+    const auto reduce = [](const Lock& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Grant& lhs, const Grant& rhs)
 {
-    return true;
+    const auto reduce = [](const Grant& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Revoke& lhs, const Revoke& rhs)
 {
-    return true;
+    const auto reduce = [](const Revoke& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Commit& lhs, const Commit& rhs)
 {
-    return true;
+    const auto reduce = [](const Commit& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Rollback& lhs, const Rollback& rhs)
 {
-    return true;
+    const auto reduce = [](const Rollback& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const SavePoint& lhs, const SavePoint& rhs)
 {
-    return true;
+    const auto reduce = [](const SavePoint& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Index& lhs, const Index& rhs)
 {
-    return true;
+    const auto reduce = [](const Index& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const Database& lhs, const Database& rhs)
 {
-    return true;
+    const auto reduce = [](const Database& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 bool operator==(const View& lhs, const View& rhs)
 {
 
-    return true;
+    const auto reduce = [](const View& obj) { return true; };
+
+    return reduce(lhs) == reduce(rhs);
 }
 
 } // namespace LunarDB::Moonlight::QueryData
