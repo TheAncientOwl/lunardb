0c7734f0a85c5f05b9e23ad033d7977a6212f343
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-12-18 00:05:48 +0200

    Provided rename parser tests

--- src/Moonlight/test/Parsers/RenameParserTest.cpp
+++ src/Moonlight/test/Parsers/RenameParserTest.cpp
@@ -3,20 +3,109 @@
 #include "Helpers/QueryDataHelpers.hpp"
 #include "ParsedQuery.hpp"
 
+#define EXPECT_SUCCESS(query, expected) \
+    const auto out = API::ParsedQuery::from(query).get<Rename>(); \
+    EXPECT_EQ(out, expected)
+
+#define EXPECT_FAIL(query) \
+    EXPECT_THROW(API::ParsedQuery::from(query).get<Rename>(), std::runtime_error)
+
 namespace LunarDB::Moonlight::Implementation::Tests {
 
 using namespace QueryData;
 
-// TODO: provide unit tests
-TEST(RenameParserTest, dummy)
+TEST(RenameParserTest, success01)
 {
-    const auto query = "rename";
+    const auto query = "rename structure from SomeStructure to SomeStructureNew";
+    const auto expected = Init::RenameInit{}
+        .type(Primitives::ERenameType::Structure)
+        .old_name("SomeStructure")
+        .new_name("SomeStructureNew");
 
-    const auto out = API::ParsedQuery::from(query).get<Rename>();
+    EXPECT_SUCCESS(query, expected);
+}
 
-    const Rename expected{};
+TEST(RenameParserTest, success02)
+{
+    const auto query = "rename structure from SomeStructure to SomeStructureNew;";
+    const auto expected = Init::RenameInit{}
+        .type(Primitives::ERenameType::Structure)
+        .old_name("SomeStructure")
+        .new_name("SomeStructureNew");
 
-    EXPECT_EQ(out, expected);
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RenameParserTest, success03)
+{
+    const auto query = "    rename      structure    from     SomeStructure         to      SomeStructureNew     ;   ";
+    const auto expected = Init::RenameInit{}
+        .type(Primitives::ERenameType::Structure)
+        .old_name("SomeStructure")
+        .new_name("SomeStructureNew");
+
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RenameParserTest, success04)
+{
+    const auto query = "rename database from SomeDatabase to SomeDatabaseNew";
+    const auto expected = Init::RenameInit{}
+        .type(Primitives::ERenameType::Database)
+        .old_name("SomeDatabase")
+        .new_name("SomeDatabaseNew");
+
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RenameParserTest, success05)
+{
+    const auto query = "    rename      database    from     SomeDatabase         to      SomeDatabaseNew     ;   ";
+    const auto expected = Init::RenameInit{}
+        .type(Primitives::ERenameType::Database)
+        .old_name("SomeDatabase")
+        .new_name("SomeDatabaseNew");
+
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RenameParserTest, success06)
+{
+    const auto query = "rename field from SomeStructure::SomeField to SomeStructure::SomeFieldNew";
+    const auto expected = Init::RenameInit{}
+        .type(Primitives::ERenameType::Field)
+        .old_name("SomeStructure::SomeField")
+        .new_name("SomeStructure::SomeFieldNew");
+
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RenameParserTest, success07)
+{
+    const auto query = "    rename      field        from       SomeStructure::SomeField   to        SomeStructure::SomeFieldNew     ;";
+    const auto expected = Init::RenameInit{}
+        .type(Primitives::ERenameType::Field)
+        .old_name("SomeStructure::SomeField")
+        .new_name("SomeStructure::SomeFieldNew");
+
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RenameParserTest, fail01)
+{
+    EXPECT_FAIL("rename structure from SomeStructure to SomeStructureNew");
+    EXPECT_FAIL("rename structure from Some Structure to SomeStructureNew");
+    EXPECT_FAIL("rename structure SomeStructure to SomeStructureNew");
+    EXPECT_FAIL("rename from SomeStructure to SomeStructureNew");
+
+    EXPECT_FAIL("rename database from SomeDatabase to SomeDatabaseNew");
+    EXPECT_FAIL("rename database from Some Database to SomeDatabaseNew");
+    EXPECT_FAIL("rename database SomeDatabase to SomeDatabaseNew");
+
+    EXPECT_FAIL("rename field from SomeField to SomeFieldNew");
+    EXPECT_FAIL("rename field from SomeStructure SomeField to SomeStructure::SomeFieldNew");
+    EXPECT_FAIL("rename field from SomeStructure::SomeField to SomeStructure SomeFieldNew");
+    EXPECT_FAIL("rename field SomeStructure::SomeField to SomeStructure::SomeFieldNew");
 }
 
 } // namespace LunarDB::Moonlight::Implementation::Tests
