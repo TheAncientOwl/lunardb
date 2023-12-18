e40975d6e668c5b66f3d0d1819d52b60397afd57
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-12-18 18:47:40 +0200

    Provided Commit unit tests

--- src/Moonlight/test/Parsers/CommitParserTest.cpp
+++ src/Moonlight/test/Parsers/CommitParserTest.cpp
@@ -3,20 +3,37 @@
 #include "Helpers/QueryDataHelpers.hpp"
 #include "ParsedQuery.hpp"
 
+#define EXPECT_SUCCESS(query, expected) \
+    const auto out = API::ParsedQuery::from(query).get<Commit>(); \
+    EXPECT_EQ(out, expected)
+
+#define EXPECT_FAIL(query) \
+    EXPECT_THROW(API::ParsedQuery::from(query).get<Commit>(), std::runtime_error)
+
 namespace LunarDB::Moonlight::Implementation::Tests {
 
 using namespace QueryData;
 
-// TODO: provide unit tests
-TEST(CommitParserTest, dummy)
+TEST(CommitParserTest, success01)
 {
     const auto query = "commit";
+    const auto expected = Init::CommitInit{}.hash(std::nullopt);
 
-    const auto out = API::ParsedQuery::from(query).get<Commit>();
+    EXPECT_SUCCESS(query, expected);
+}
 
-    const Commit expected{};
+TEST(CommitParserTest, success02)
+{
+    const auto query = "commit;";
+    const auto expected = Init::CommitInit{}.hash(std::nullopt);
 
-    EXPECT_EQ(out, expected);
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(CommitParserTest, fail01)
+{
+    EXPECT_FAIL("commit a591a6d40b f420404a011733cfb7b19 0d62c65bf0bcda32b57b27 7d9ad9f146e");
+    EXPECT_FAIL("commit a591a6d40b f420404a011733cfb7b19 0d62c65bf0bcda32b57b27 7d9ad9f146e;");
 }
 
 } // namespace LunarDB::Moonlight::Implementation::Tests
