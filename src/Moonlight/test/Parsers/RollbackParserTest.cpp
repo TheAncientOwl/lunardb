74f09f5231696d478e45a184d725fb4818a6a3e0
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-12-18 18:48:03 +0200

    Provided Rollback unit tests

--- src/Moonlight/test/Parsers/RollbackParserTest.cpp
+++ src/Moonlight/test/Parsers/RollbackParserTest.cpp
@@ -3,20 +3,53 @@
 #include "Helpers/QueryDataHelpers.hpp"
 #include "ParsedQuery.hpp"
 
+#define EXPECT_SUCCESS(query, expected) \
+    const auto out = API::ParsedQuery::from(query).get<Rollback>(); \
+    EXPECT_EQ(out, expected)
+
+#define EXPECT_FAIL(query) \
+    EXPECT_THROW(API::ParsedQuery::from(query).get<Rollback>(), std::runtime_error)
+
 namespace LunarDB::Moonlight::Implementation::Tests {
 
 using namespace QueryData;
 
-// TODO: provide unit tests
-TEST(RollbackParserTest, dummy)
+TEST(RollbackParserTest, success01)
 {
     const auto query = "rollback";
+    const auto expected = Init::RollbackInit{}.hash(std::nullopt);
 
-    const auto out = API::ParsedQuery::from(query).get<Rollback>();
+    EXPECT_SUCCESS(query, expected);
+}
 
-    const Rollback expected{};
+TEST(RollbackParserTest, success02)
+{
+    const auto query = "rollback;";
+    const auto expected = Init::RollbackInit{}.hash(std::nullopt);
 
-    EXPECT_EQ(out, expected);
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RollbackParserTest, success03)
+{
+    const auto query = "rollback a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e";
+    const auto expected = Init::RollbackInit{}.hash("a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e");
+
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RollbackParserTest, success04)
+{
+    const auto query = "rollback a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e;";
+    const auto expected = Init::RollbackInit{}.hash("a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e");
+
+    EXPECT_SUCCESS(query, expected);
+}
+
+TEST(RollbackParserTest, fail01)
+{
+    EXPECT_FAIL("rollback a591a6d40b f420404a011733cfb7b19 0d62c65bf0bcda32b57b27 7d9ad9f146e");
+    EXPECT_FAIL("rollback a591a6d40b f420404a011733cfb7b19 0d62c65bf0bcda32b57b27 7d9ad9f146e;");
 }
 
 } // namespace LunarDB::Moonlight::Implementation::Tests
