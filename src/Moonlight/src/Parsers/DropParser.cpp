5ddc40c1b874f4df658faaa9ce247d7ab313c764
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-12-20 01:56:09 +0200

    Provided Drop parser implementation

--- src/Moonlight/src/Parsers/DropParser.cpp
+++ src/Moonlight/src/Parsers/DropParser.cpp
@@ -1,4 +1,5 @@
 #include "QueryParsers.hpp"
+#include "QueryExtractor.hpp"
 #include "Utils.hpp"
 
 namespace LunarDB::Moonlight::Implementation {
@@ -15,8 +16,35 @@ constexpr auto c_query_prefix{ "drop" };
 PROVIDE_QUERY_PARSER_IMPL(Drop, c_query_prefix)
 {
     DECLARE_PARSED_QUERY(obj, Drop);
+    QueryExtractor extractor{ query };
 
-    // TODO: provide implementation
+    const auto [drop, structure, structure_name, cascade] = extractor.extractTuple<4>();
+    if (!extractor.empty()) { throw Utils::buildInvalidQueryFormatError(c_query_prefix); }
+
+    Utils::checkKeyword(drop, "drop");
+    Utils::checkKeyword(structure, "structure");
+
+    if (structure_name.empty())
+    {
+        throw Utils::buildMissingError("structure name");
+    }
+    else
+    {
+        obj.structure_name = structure_name;
+    }
+
+    if (cascade.empty())
+    {
+        obj.cascade = false;
+    }
+    else if (Utils::equalsIgnoreCase(cascade, "cascade"))
+    {
+        obj.cascade = true;
+    }
+    else
+    {
+        throw Utils::buildUnknownKeywordError(cascade);
+    }
 
     RETURN_PARSED_QUERY;
 }
