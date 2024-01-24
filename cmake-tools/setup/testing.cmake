5c1a9bd2d5cd601f0b238464a1a447af6cba5c61
TheAncientOwl <dele.alex.o.o@gmail.com>
2024-01-24 14:34:41 +0200

    Added toggles for unit tests building

--- cmake-tools/setup/testing.cmake
+++ cmake-tools/setup/testing.cmake
@@ -1,18 +1,22 @@
-include(FetchContent)
+include(cmake-tools/setup/private/testing_modules.cmake)
 
-FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG v1.14.0
-)
+if (LUNAR_ENABLE_TESTING)
+    include(FetchContent)
 
-FetchContent_GetProperties(googletest)
-if(NOT googletest_POPULATED)
-    FetchContent_Populate(googletest)
-    add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
+    FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG v1.14.0
+    )
+
+    FetchContent_GetProperties(googletest)
+    if(NOT googletest_POPULATED)
+        FetchContent_Populate(googletest)
+        add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
+    endif()
+
+    include(GoogleTest)
+
+    include(CTest)
+    enable_testing()
 endif()
-
-include(GoogleTest)
-
-include(CTest)
-enable_testing()
